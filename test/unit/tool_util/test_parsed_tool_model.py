e7e94ce1b4cdce0096d6f9ee7a6c143c92fa96a2
John Chilton <jmchilton@gmail.com>
2026-05-03 12:03:52 -0400

    Use functional tools for ParsedTool tests

--- test/unit/tool_util/test_parsed_tool_model.py
+++ test/unit/tool_util/test_parsed_tool_model.py
@@ -2,104 +2,92 @@ from galaxy.tool_util.model_factory import (
     parse_tool,
     parse_tool_custom,
 )
-from galaxy.tool_util.parser.factory import build_xml_tool_source
-from galaxy.tool_util_models.tool_source import PackageRequirement
+from galaxy.tool_util.parser.factory import get_tool_source
+from galaxy.tool_util.unittest_utils import functional_test_tool_path
 from tool_shed_client.schema import ShedParsedTool
 
 
+def tool_source_for(tool_name: str):
+    return get_tool_source(functional_test_tool_path(tool_name))
+
+
+def parsed_tool_for(tool_name: str):
+    return parse_tool(tool_source_for(tool_name))
+
+
 def test_parsed_tool_exposes_package_requirements():
-    tool = parse_tool(build_xml_tool_source("""
-<tool id="parsed_req" name="Parsed Requirement" version="1.0" profile="22.05">
-    <requirements>
-        <requirement type="package" version="1.17">samtools</requirement>
-    </requirements>
-</tool>
-"""))
+    tool = parsed_tool_for("mulled_example_explicit.xml")
 
     assert len(tool.requirements) == 1
     requirement = tool.requirements[0]
     assert requirement.type == "package"
-    assert requirement.name == "samtools"
-    assert requirement.version == "1.17"
+    assert requirement.name == "bwa"
+    assert requirement.version == "0.7.15"
 
 
 def test_parsed_tool_exposes_containers():
-    tool = parse_tool(build_xml_tool_source("""
-<tool id="parsed_container" name="Parsed Container" version="1.0" profile="22.05">
-    <requirements>
-        <container type="docker">quay.io/biocontainers/samtools:1.17--h00cdaf9_0</container>
-    </requirements>
-</tool>
-"""))
+    tool = parsed_tool_for("mulled_example_explicit.xml")
 
     assert len(tool.containers) == 1
     container = tool.containers[0]
     assert container.type == "docker"
-    assert container.container_id == "quay.io/biocontainers/samtools:1.17--h00cdaf9_0"
+    assert container.container_id == "quay.io/biocontainers/bwa:0.7.15--0"
 
 
-def test_parsed_tool_exposes_resource_requirements_without_defaults():
-    tool = parse_tool(build_xml_tool_source("""
-<tool id="parsed_resource" name="Parsed Resource" version="1.0" profile="22.05">
-    <requirements>
-        <resource type="cores_min">4</resource>
-    </requirements>
-</tool>
-"""))
+def test_parsed_tool_exposes_resource_requirements():
+    tool = parsed_tool_for("resource_requirements.xml")
 
     assert len(tool.requirements) == 1
     requirement = tool.requirements[0]
     assert requirement.type == "resource"
-    assert requirement.cores_min == "4"
-    assert requirement.ram_min is None
+    assert requirement.cores_min == "1.1"
+    assert requirement.cores_max == "2"
+    assert requirement.ram_min == "1.1"
+    assert requirement.ram_max == "2>"
+    assert requirement.tmpdir_min == "$(inputs.input1.size)"
+    assert requirement.tmpdir_max == "$(inputs.input1.size * 2)"
+    assert requirement.timelimit == "60"
 
 
-def test_package_requirement_version_is_optional():
-    requirement = PackageRequirement.model_validate({"type": "package", "name": "bwa"})
+def test_parsed_tool_exposes_versionless_package_requirements():
+    tool = parsed_tool_for("mulled_example_multi_versionless.xml")
 
-    assert requirement.version is None
+    assert len(tool.requirements) == 2
+    requirements_by_name = {requirement.name: requirement for requirement in tool.requirements}
+    assert requirements_by_name["samtools"].version is None
+    assert requirements_by_name["bedtools"].version is None
 
 
-def test_parsed_tool_exposes_stdio_rules():
-    tool = parse_tool(build_xml_tool_source("""
-<tool id="parsed_stdio" name="Parsed Stdio" version="1.0" profile="22.05">
-    <stdio>
-        <exit_code range="2:4" level="fatal" description="bad exit" />
-        <regex match="WARNING" source="stdout" level="warning" description="warn text" />
-    </stdio>
-</tool>
-"""))
+def test_parsed_tool_exposes_stdio_exit_code_rules():
+    tool = parsed_tool_for("mulled_example_explicit.xml")
 
     assert len(tool.stdio.exit_codes) == 1
     exit_code = tool.stdio.exit_codes[0]
-    assert exit_code.range_start == 2
-    assert exit_code.range_end == 4
+    assert exit_code.range_start == 2.0
+    assert exit_code.range_end == "inf"
     assert exit_code.error_level == 3
-    assert exit_code.desc == "bad exit"
 
-    assert len(tool.stdio.regexes) == 1
+
+def test_parsed_tool_exposes_stdio_regex_rules():
+    tool = parsed_tool_for("detect_errors.xml")
+
+    assert len(tool.stdio.exit_codes) == 3
+    assert len(tool.stdio.regexes) == 6
     regex = tool.stdio.regexes[0]
-    assert regex.match == "WARNING"
+    assert regex.match == "message"
     assert regex.stdout_match is True
     assert regex.stderr_match is False
-    assert regex.error_level == 2
-    assert regex.desc == "warn text"
+    assert regex.error_level == 1
+    assert regex.desc == "some program message of interest"
 
 
 def test_parsed_tool_and_shed_parsed_tool_serialize():
-    tool_source = build_xml_tool_source("""
-<tool id="parsed_serialized" name="Parsed Serialized" version="1.0" profile="22.05">
-    <requirements>
-        <requirement type="package" version="1.17">samtools</requirement>
-        <container type="docker">quay.io/biocontainers/samtools:1.17--h00cdaf9_0</container>
-    </requirements>
-</tool>
-""")
+    tool_source = tool_source_for("mulled_example_explicit.xml")
 
     parsed_tool = parse_tool(tool_source)
     shed_parsed_tool = parse_tool_custom(tool_source, ShedParsedTool)
 
-    assert parsed_tool.model_dump(mode="json")["requirements"][0]["name"] == "samtools"
+    assert parsed_tool.model_dump(mode="json")["requirements"][0]["name"] == "bwa"
     assert shed_parsed_tool.model_dump(mode="json")["containers"][0]["type"] == "docker"
     assert parsed_tool.model_dump_json()
     assert shed_parsed_tool.model_dump_json()
